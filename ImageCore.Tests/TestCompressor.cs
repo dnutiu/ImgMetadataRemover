c6f1ec0a37fe3935a2a89d964a9631a68dc728f3
Denis Nutiu <denis.nutiu@gmail.com>
2022-01-25 22:19:56 +0200

    Implement TestCompressor.cs

---
+++ ImageCore.Tests/TestCompressor.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using Image;
+using Xunit;
+
+namespace ImageCore.Tests
+{
+    public class TestLosslessCompressor
+    {
+        private readonly string _testsProjectDirectory;
+
+        public TestLosslessCompressor()
+        {
+            _testsProjectDirectory = Environment.GetEnvironmentVariable("IMAGE_CORE_TESTS");
+        }
+
+        [Fact]
+        public void TestLosslessCompressor_Compress()
+        {
+            ICompressor compressor = new LosslessCompressor();
+            var sourceFileName = Path.Join(_testsProjectDirectory, "test_pictures/IMG_0138.HEIC");
+            var destinationFileName = Path.GetTempFileName();
+            File.Copy(sourceFileName, destinationFileName, true);
+            compressor.Compress(destinationFileName);
+
+            var originalFile = File.Open(sourceFileName, FileMode.Open);
+            var compressedFile = File.Open(destinationFileName, FileMode.Open);
+            
+            Assert.True(compressedFile.Length <= originalFile.Length);
+            
+            originalFile.Close();
+            compressedFile.Close();
+            File.Delete(destinationFileName);
+        }
+        
+        [Fact]
+        public void TestNullCompressor_Compress()
+        {
+            ICompressor compressor = new NullCompressor();
+            var sourceFileName = Path.Join(_testsProjectDirectory, "test_pictures/IMG_0138.HEIC");
+            var destinationFileName = Path.GetTempFileName();
+            File.Copy(sourceFileName, destinationFileName, true);
+            compressor.Compress(destinationFileName);
+            
+            var originalFile = File.Open(sourceFileName, FileMode.Open);
+            var compressedFile = File.Open(destinationFileName, FileMode.Open);
+            
+            Assert.True(compressedFile.Length == originalFile.Length);
+            
+            originalFile.Close();
+            compressedFile.Close();
+            File.Delete(destinationFileName);
+        }
+    }
+}
