292d8e0dac5aba136fd1010517c2ae42d6c90bd3
Denis Nutiu <denis.nutiu@gmail.com>
2022-01-25 22:57:40 +0200

    Implement tests for MetadataRemover

---
+++ ImageCore.Tests/TestMetadataRemover.cs
@@ -0,0 +1,28 @@
+﻿using Image.Compressor;
+using Image.Remover;
+using ImageMagick;
+using Moq;
+using Xunit;
+
+namespace ImageCore.Tests
+{
+    public class TestMetadataRemover
+    {
+        [Fact]
+        public void TestCleanImage()
+        {
+            // Setup
+            var magicImageMock = new Mock<IMagickImage>();
+            var compressorMock = new Mock<ICompressor>();
+            var metadataRemover = new ExifMetadataRemoverAndCompressor(magicImageMock.Object, compressorMock.Object);
+            
+            // Test
+            metadataRemover.CleanImage("path");
+            
+            // Assert
+            magicImageMock.Verify( i => i.RemoveProfile("exif"));
+            magicImageMock.Verify( i => i.Write("path"));
+            compressorMock.Verify( i => i.Compress("path"));
+        }
+    }
+}
