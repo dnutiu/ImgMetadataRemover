ac373509404d60daa60064333b2b3becb914877c
Denis Nutiu <denis.nutiu@gmail.com>
2022-02-04 21:21:27 +0200

    Migrate demo app to WinUI/WindowsSDK

--- WindowsApplication/MainWindow.xaml.cs
+++ WindowsApplication/MainWindow.xaml.cs
@@ -1,27 +1,34 @@
-﻿using System;
+﻿using Microsoft.UI.Xaml;
+using System;
 using System.Collections.Generic;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
+using System.Runtime.InteropServices;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
 
-namespace UwpApplication
+namespace WindowsApplication
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// An empty window that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainWindow : Window
     {
-        public MainPage()
+        public MainWindow()
         {
             this.InitializeComponent();
-            Console.WriteLine("Main Page");
-            this.Loaded += LoadedHandler;
+            UpdateNumberOfFilesText(0);
         }
 
-        private async void LoadedHandler(object sender, RoutedEventArgs e)
+        private async void PickFilesButton_Click(object sender, RoutedEventArgs e)
         {
             var folderPicker = new Windows.Storage.Pickers.FolderPicker();
+
+            // Get the current window's HWND by passing in the Window object
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(this);
+
+            // Associate the HWND with the file picker
+            WinRT.Interop.InitializeWithWindow.Initialize(folderPicker, hwnd);
+
             folderPicker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.Desktop;
             folderPicker.FileTypeFilter.Add("*");
 
@@ -42,7 +49,13 @@ namespace UwpApplication
                     }
                 }
                 FilesListView.ItemsSource = fileNames;
+                UpdateNumberOfFilesText(fileNames.Count);
             }
+        }
+
+        private void UpdateNumberOfFilesText(int nrOfFiles)
+        {
+            NumberOfFiles.Text = $"Total files {nrOfFiles}.";
         }
     }
 }
