89fc37debaa41110222b371abfe51fc8162beae3
Denis Nutiu <denis.nutiu@gmail.com>
2022-02-09 22:49:14 +0200

    Remove ListBox_PreviewMouseWheel from MainWindow.cs

--- DesktopApplication/MainWindow.xaml.cs
+++ DesktopApplication/MainWindow.xaml.cs
@@ -53,18 +53,5 @@ namespace DesktopApplication
             Console.WriteLine("CleanFilesBtn_OnClick - On Click");
 
         }
-        
-        private void ListBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if (e.Delta > 0)
-            {
-                ScrollBar.LineDownCommand.Execute(null, e.OriginalSource as IInputElement);
-            }
-            if (e.Delta < 0)
-            {
-                ScrollBar.LineUpCommand.Execute(null, e.OriginalSource as IInputElement);
-            }
-            e.Handled = true;
-        }
     }
 }
